dab2c20397f080a907414da37b990dbf6185a97d
eltiko <elticco@hotmail.com>
2019-10-01 15:54:23 -0700

    fix

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-'use strict'
+
 
 
 var username = prompt ('what\'s your name');
-            console.log ('username', username); 
-            alert ('Hello' + ' ' + username); 
+console.log ('username', username);
+alert ('Hello' + ' ' + username);
 var study = prompt ('did you go to the college');
 study = study.toUpperCase();
 if (study === 'YES') {
-    alert ( 'Congrats' + ' ' + username );
+  alert ( 'Congrats' + ' ' + username );
 } else {
-    alert ( 'you should to it' + ' ' + username)
+  alert ( 'you should to it' + ' ' + username);
 }
-var studyPlus = prompt ( 'Dou you like sciences')
+var studyPlus = prompt ( 'Dou you like sciences');
 studyPlus = studyPlus.toUpperCase();
 if (studyPlus === 'YES'){
-    alert ('amazing')
+  alert ('amazing');
 } else {
-   alert('Oh' + '' + username + '' + 'science makes the word go around'); 
+  alert('Oh' + '' + username + '' + 'science makes the word go around');
 }var study2 = prompt ( 'Do you like biology');
 study2 = study2.toUpperCase();
 if (study2 === 'YES'){
-    alert ( 'Welcome to my world');
+  alert ( 'Welcome to my world');
 } else {
-  alert  ('Let me tell that you biology is life');
+  alert ('Let me tell that you biology is life');
 }
 var job = prompt ( 'did you ever been teacher' );
 job = job.toUpperCase ();
 if ( job === 'YES') {
-    alert ('you did the best thing in the world' + '' + username);
+  alert ('you did the best thing in the world' + '' + username);
 } else {
-   alert  ('knowledge is the only thing you can share without loosing it');
+  alert ('knowledge is the only thing you can share without loosing it');
 }
 var cook = prompt ( 'Do you know how to cook');
 cook = cook.toUpperCase ();
 if (cook === 'YES') {
-    ('Your partener will love you so much as your kids');
+  altert ('Your partener will love you so much as your kids');
 } else {
-  alert  ('You should learn it for youself, your partener and your kids of course');
-}
+  alert ('You should learn it for youself, your partener and your kids of course');
+} 
