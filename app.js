ede990d6dd610ae3ba45136ec5a3f46971959646
eltiko <elticco@hotmail.com>
2019-10-02 14:28:23 -0700

    for loop

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ if (study2 === 'YES' || study2 === 'Y'){
 var job = prompt ( 'did you ever been teacher' );
 job = job.toUpperCase ();
 if ( job === 'YES' || job === 'Y') {
-  alert ('you did the best thing in the world' + '' + username);
+  alert ('you did the best thing in the world' + ' ' + username);
 } else {
   alert ('knowledge is the only thing you can share without loosing it');
 }
@@ -37,4 +37,19 @@ if (cook === 'YES' || cook === 'Y') {
   altert ('Your partener will love you so much as your kids');
 } else {
   alert ('You should learn it for youself, your partener and your kids of course');
-} 
+}
+for (var i = 0; i < 4 ; i++)  {
+  var secretNumber = '10';
+  var userNumberGuess = prompt ('well ! Lets play ! Can you guess my favorite number');
+  if (userNumberGuess === secretNumber){
+    alert ('Great you should be a magician' + ' ' + username);
+      break; 
+  } 
+  else if ( userNumberGuess > secretNumber){
+    alert ('nope ! it\'s lower');
+  } else if (userNumberGuess < secretNumber){
+    alert ('i thing it\' higher than that')
+}
+}
+
+  
